6dda0fb03b945876e88404cf15cf2cd6e8475311
Patrick Li <patrickli727@gmail.com>
2016-01-06 19:49:37 -0800

    First commit.

---
+++ machine.js
@@ -0,0 +1,5 @@
+
+$(function () {
+  var rootEl = document.getElementById('main');
+
+});
---
+++ webpack.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  context: __dirname,
+  entry: "./machine.js",
+  output: {
+    path: "./js",
+    publicPath: "/js/",
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-maps'
+};
